3ab87281037fc6a90ff49375ab3e3fa8e133b0a1
Roberto Bezzi <rbezzi@gmail.com>
2014-04-17 23:59:20 +0100

    Maven only template - no Eclipse

---
+++ src/main/resources/archetype-resources/src/test/java/test/AppTest.java
@@ -0,0 +1,16 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void appTest() throws Exception {
+		assertThat(true, is(true));
+	}
+}
